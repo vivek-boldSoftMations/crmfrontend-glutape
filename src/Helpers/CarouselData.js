16a036a577defb0acffe695aebfcb6cb5cc202e2
vivek-boldSoftMations <vivek@glutape.com>
2023-01-05 11:41:30 +0530

    feat L added caousel

---
+++ src/Helpers/CarouselData.js
@@ -0,0 +1,29 @@
+import Slider1 from "../Images/gt_slide01.jpg";
+import Slider2 from "../Images/gt_slide02.jpg";
+import Slider3 from "../Images/gt_slide03.jpg";
+import Slider4 from "../Images/gt_slide04.jpg";
+import Slider5 from "../Images/gt_slide05.jpg";
+import Slider6 from "../Images/gt_slide06.jpg";
+import Slider7 from "../Images/gt_slide07.jpg";
+import Slider8 from "../Images/gt_slide08.jpg";
+import Slider9 from "../Images/gt_slide09.jpg";
+
+export const images = [
+  { title: "Glutape", subtitle: "ACHEM WONDER", img: Slider1 },
+  {
+    title: "Glutape",
+    subtitle: "BIESSSE",
+    img: Slider2,
+  },
+  {
+    title: "Glutape",
+    subtitle: "ESTAPE",
+    img: Slider3,
+  },
+  { title: "Glutape", subtitle: "GRIPPEX", img: Slider4 },
+  { title: "Glutape", subtitle: "GRIPPO", img: Slider5 },
+  { title: "Glutape", subtitle: "IKULOR", img: Slider6 },
+  { title: "Glutape", subtitle: "SEAL KING", img: Slider7 },
+  { title: "Glutape", subtitle: "KEEP DISTANCE, STAY TOGETHER", img: Slider8 },
+  { title: "Glutape", subtitle: "PVC FLOOR MARKING", img: Slider9 },
+];
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,8 +1,9 @@
+
 import React, { useState } from "react";
-import "./Dashboard.css";
-// import { images } from "../../Helpers/CarouselData";
-// import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
-// import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import "./Home.css";
+import { images } from "../../Helpers/CarouselData";
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import { Carousel } from 'react-bootstrap';
 import Slider1 from "../../Images/gt_slide01.jpg";
 import Slider2 from "../../Images/gt_slide02.jpg";
@@ -13,19 +14,20 @@ import Slider6 from "../../Images/gt_slide06.jpg";
 import Slider7 from "../../Images/gt_slide07.jpg";
 import Slider8 from "../../Images/gt_slide08.jpg";
 import Slider9 from "../../Images/gt_slide09.jpg";
-
-export function Dashboard() {
+export const Home = () => {
   const [currImg, setCurrImg] = useState(0);
-  const [index, setIndex] = useState(0);
+console.log('images :>> ', images);
+    const [index, setIndex] = useState(0);
 
   const handleSelect = (selectedIndex, e) => {
     setIndex(selectedIndex);
   };
+
   return (
-    <Carousel className="h-50" fade activeIndex={index} onSelect={handleSelect}>
-    <Carousel.Item interval={500}>
+    <Carousel fade activeIndex={index} onSelect={handleSelect}>
+    <Carousel.Item>
       <img
-        className="d-block"
+        // className="d-block 
         src={Slider1}
         alt="First slide"
       />
@@ -34,7 +36,7 @@ export function Dashboard() {
         <h3>ACHEM WONDER</h3>
       </Carousel.Caption>
     </Carousel.Item>
-    <Carousel.Item interval={500}> 
+    <Carousel.Item > 
       <img
         className="d-block "
         src={Slider2}
@@ -46,7 +48,7 @@ export function Dashboard() {
         <h3>BIESSSE</h3>
       </Carousel.Caption>
     </Carousel.Item>
-    <Carousel.Item interval={500}>
+    <Carousel.Item >
       <img
         className="d-block "
         src={Slider3}
@@ -59,7 +61,7 @@ export function Dashboard() {
       </Carousel.Caption>
       
     </Carousel.Item>
-    <Carousel.Item interval={500}>
+    <Carousel.Item >
       <img
         className="d-block "
         src={Slider4}
@@ -71,7 +73,7 @@ export function Dashboard() {
         <h3>GRIPPEX</h3>
       </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item interval={500}>
+      <Carousel.Item >
       <img
         className="d-block "
         src={Slider5}
@@ -83,7 +85,7 @@ export function Dashboard() {
         <h3>GRIPPO</h3>
       </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item interval={500}>
+      <Carousel.Item >
       <img
         className="d-block "
         src={Slider6}
@@ -95,7 +97,7 @@ export function Dashboard() {
         <h3>IKULOR</h3>
       </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item interval={500}>
+      <Carousel.Item >
       <img
         className="d-block "
         src={Slider7}
@@ -107,7 +109,7 @@ export function Dashboard() {
         <h3>SEAL KING</h3>
       </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item interval={500}>
+      <Carousel.Item >
       <img
         className="d-block "
         src={Slider8}
@@ -120,7 +122,7 @@ export function Dashboard() {
       </Carousel.Caption>
       
     </Carousel.Item>
-    <Carousel.Item interval={500}x>
+    <Carousel.Item >
       <img
         className="d-block "
         src={Slider9}
@@ -136,5 +138,34 @@ export function Dashboard() {
     </Carousel.Item>
 
   </Carousel>
+  
+    // <div className="carousel">
+    //   <div
+    //     className="carouselInner"
+    //     style={{ backgroundImage: `url(${images[currImg].img})` }}
+    //   >
+    //     <div
+    //       className="left"
+    //       onClick={() => {
+    //         currImg > 0 && setCurrImg(currImg - 1);
+    //       }}
+    //     >
+    //       <ArrowBackIosNewIcon style={{ fontSize: 30 }} />
+    //     </div>
+    //     <div className="center">
+    //       <h1>{images[currImg].title}</h1>
+    //       <p>{images[currImg].subtitle}</p>
+    //     </div>
+    //     <div
+    //       className="right"
+    //       onClick={() => {
+    //         currImg < images.length - 1 && setCurrImg(currImg + 1);
+    //       }}
+    //     >
+    //       <ArrowForwardIosIcon style={{ fontSize: 30 }} />
+    //     </div>
+    //   </div>
+    // </div>
+
   );
-}
+};
