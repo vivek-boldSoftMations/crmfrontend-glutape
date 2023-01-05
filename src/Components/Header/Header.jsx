e99646363f90457ce0258142c37efd17075d37bc
vivek-boldSoftMations <vivek@glutape.com>
2023-01-05 11:40:31 +0530

    feat added listitem file to header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,4 +1,7 @@
+import { Link as RouterLink } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import React, { useState } from "react";
+
 import {
   Button,
   AppBar,
@@ -6,21 +9,17 @@ import {
   Typography,
   IconButton,
   List,
-  ListItem,
-  ListItemText,
   CssBaseline,
+  Divider,
+  Drawer
 } from "@mui/material";
 import { styled, useTheme } from "@mui/material/styles";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import MenuIcon from "@mui/icons-material/Menu";
-import { Link as RouterLink } from "react-router-dom";
-import { Collapse, Divider, Drawer } from "@mui/material";
-import ExpandLessIcon from "@mui/icons-material/ExpandLess";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import { useDispatch, useSelector } from "react-redux";
 import { logoutUser } from "../../Redux/Action/Action";
 import "./Header.css";
+import { ListItems } from './ListItems';
 
 const DrawerHeader = styled("div")(({ theme }) => ({
   display: "flex",
@@ -35,38 +34,22 @@ export const Header = () => {
   const [open, setOpen] = useState(false);
   const theme = useTheme();
   const dispatch = useDispatch();
-  const [expand, setExpand] = useState(false);
-  const [expandFollowUp, setExpandFollowUp] = useState(false);
-  const [expandProduct, setExpandProduct] = useState(false);
-  const [expandCustomer, setExpandCustomer] = useState(false);
-  const [expandInvoice, setExpandInvoice] = useState(false);
-  const [expandOrderBook, setExpandOrderBook] = useState(false);
-  const [sellerAccount, setSellerAccount] = useState(false);
   const auth = useSelector((state) => state.auth);
-  const [isHover, setIsHover] = useState(false);
-  // const auth = sessionStorage.getItem("accessToken");
-  const boxStyle = {
-    backgroundColor: isHover ? "rgb(0, 191, 255)" : "rgb(0, 191, 255)",
-  };
+  const [isHoverLogout, setIsHoverLogout] = useState(false);
 
   const handleDrawerClose = () => {
     setOpen(false);
-  };
-
-  const handleMouseEnter = () => {
-    setIsHover(true);
-  };
-  const handleMouseLeave = () => {
-    setIsHover(false);
   };
 
   const logout = () => {
     dispatch(logoutUser());
     localStorage.removeItem("user");
   };
+
   return (
     <div>
       <CssBaseline />
+      {/* sidebar */}
       <Drawer open={open} onClose={() => setOpen(false)}>
         <DrawerHeader>
           <IconButton onClick={handleDrawerClose}>
@@ -83,275 +66,11 @@ export const Header = () => {
           style={{ width: "100%", marginBottom: "2em" }}
           disablePadding
         >
-          {/* Products */}
-          <ListItem button onClick={() => setExpandProduct(!expandProduct)} style={{ width: 300 }}>
-            {/* <ListItemIcon style={menuItemIcon}></ListItemIcon> */}
-            <ListItemText primary="Products" />
-            {expandProduct ? <ExpandLessIcon /> : <ExpandMoreIcon />}
-          </ListItem>
-          <Collapse in={expandProduct} timeout="auto" unmountOnExit>
-            <Divider />
-            <List component="div" disablePadding>
-              <ListItem
-                button
-                component={RouterLink}
-                to="/products/view-colors"
-                style={{ width: 300 }}
-              >
-                <ListItemText inset primary="Colors" />
-              </ListItem>
-              <ListItem
-                button
-                component={RouterLink}
-                to="/products/view-brand"
-                style={{ width: 300 }}
-              >
-                <ListItemText inset primary="Brand" />
-              </ListItem>
-              <ListItem
-                button
-                component={RouterLink}
-                to="/products/view-basic-unit"
-                style={{ width: 300 }}
-              >
-                <ListItemText inset primary="Basic Unit" />
-              </ListItem>
-              <ListItem
-                button
-                component={RouterLink}
-                to="/products/view-unit"
-                style={{ width: 300 }}
-              >
-                <ListItemText inset primary="Unit" />
-              </ListItem>
-              <ListItem
-                button
-                component={RouterLink}
-                to="/products/view-packing-unit"
-                style={{ width: 300 }}
-              >
-                <ListItemText inset primary="Packing Unit" />
-              </ListItem>
-              <ListItem
-                button
-                component={RouterLink}
-                to="/products/view-description"
-                style={{ width: 300 }}
-              >
-                <ListItemText inset primary="Description" />
-              </ListItem>
-              <ListItem
-                button
-                component={RouterLink}
-                to="/products/view-product-code"
-                style={{ width: 300 }}
-              >
-                <ListItemText inset primary="Product Code" />
-              </ListItem>
-              <ListItem
-                button
-                component={RouterLink}
-                to="/products/view-consumable"
-                style={{ width: 300 }}
-              >
-                <ListItemText inset primary="Consumable" />
-              </ListItem>
-              <ListItem
-                button
-                component={RouterLink}
-                to="/products/view-finish-goods"
-                style={{ width: 300 }}
-              >
-                <ListItemText inset primary="Finish Goods" />
-              </ListItem>
-              <ListItem
-                button
-                component={RouterLink}
-                to="/products/view-raw-materials"
-                style={{ width: 300 }}
-              >
-                <ListItemText inset primary="Raw Materials" />
-              </ListItem>
-              <ListItem
-                button
-                component={RouterLink}
-                to="/products/view-price-list"
-                style={{ width: 300 }}
-              >
-                <ListItemText inset primary="Price List" />
-              </ListItem>
-            </List>
-          </Collapse>
-          {/* Leads */}
-          <ListItem button onClick={() => setExpand(!expand)} style={{ width: 300 }}>
-            <ListItemText primary="Leads" />
-            {expand ? <ExpandLessIcon /> : <ExpandMoreIcon />}
-          </ListItem>
-          <Collapse in={expand} timeout="auto" unmountOnExit>
-            <Divider />
-            <List component="div" disablePadding>
-              <ListItem
-                button
-                component={RouterLink}
-                to="/leads/view-lead"
-                style={{ width: 300 }}
-              >
-                <ListItemText inset primary="Lead" />
-              </ListItem>
-              <ListItem
-                button
-                component={RouterLink}
-                to="/leads/view-assignedto"
-                style={{ width: 300 }}
-              >
-                <ListItemText inset primary="Assigned To" />
-              </ListItem>
-              <ListItem
-                button
-                onClick={() => setExpandFollowUp(!expandFollowUp)}
-                style={{ width: 300 }}
-              >
-                <ListItemText inset primary="FollowUp" />
-                {expandFollowUp ? <ExpandLessIcon /> : <ExpandMoreIcon />}
-              </ListItem>
-              <Collapse in={expandFollowUp} timeout="auto" unmountOnExit>
-                <Divider />
-                <List component="div" disablePadding>
-                  <ListItem
-                    button
-                    component={RouterLink}
-                    to="/leads/view-today-followup"
-                    style={{ width: 300 }}
-                  >
-                    <ListItemText inset primary="Today FollowUp" />
-                  </ListItem>
-                  <ListItem
-                    button
-                    component={RouterLink}
-                    to="/leads/view-pending-followup"
-                    style={{ width: 300 }}
-                  >
-                    <ListItemText inset primary="Pending FollowUp" />
-                  </ListItem>
-                  <ListItem
-                    button
-                    component={RouterLink}
-                    to="/leads/view-upcoming-followup"
-                    style={{ width: 300 }}
-                  >
-                    <ListItemText inset primary="Upcoming FollowUp" />
-                  </ListItem>
-                </List>
-              </Collapse>
-            </List>
-          </Collapse>
-          {/* Customer */}
-          <ListItem button onClick={() => setExpandCustomer(!expandCustomer)} style={{ width: 300 }}>
-            <ListItemText primary="Customer" />
-            {expandCustomer ? <ExpandLessIcon /> : <ExpandMoreIcon />}
-          </ListItem>
-          <Collapse in={expandCustomer} timeout="auto" unmountOnExit>
-            <Divider />
-            <List component="div" disablePadding>
-              <ListItem
-                button
-                component={RouterLink}
-                to="/customers/company-details"
-                style={{ width: 300 }}
-              >
-                <ListItemText inset primary="Company Details" />
-              </ListItem>
-            </List>
-          </Collapse>
-              {/* Invoice  */}
-              <ListItem button onClick={() => setExpandInvoice(!expandInvoice)} style={{ width: 300 }}>
-            <ListItemText primary="Invoice" />
-            {expandInvoice ? <ExpandLessIcon /> : <ExpandMoreIcon />}
-          </ListItem>
-          <Collapse in={expandInvoice} timeout="auto" unmountOnExit>
-            <Divider />
-            <List component="div" disablePadding>
-              {/* <ListItem
-                button
-                component={RouterLink}
-                to="/invoice/seller-account"
-                style={{ width: 300 }}
-              >
-                <ListItemText inset primary="Seller Account" />
-              </ListItem> */}
-              <ListItem
-                button
-                component={RouterLink}
-                to="/invoice/performa-invoice"
-                style={{ width: 300 }}
-              >
-                <ListItemText inset primary="Customer Performa Invoice" />
-              </ListItem>
-              <ListItem
-                button
-                component={RouterLink}
-                to="/invoice/leads-performa-invoice"
-                style={{ width: 300 }}
-              >
-                <ListItemText inset primary="Leads Performa Invoice" />
-              </ListItem>
-              <ListItem
-                button
-                component={RouterLink}
-                to="/invoice/sales-invoice"
-                style={{ width: 300 }}
-              >
-                <ListItemText inset primary="Sales Invoice" />
-              </ListItem>
-            </List>
-          </Collapse>
-                   {/* Seller Account */}
-                   <ListItem button onClick={() => setSellerAccount(!sellerAccount)} style={{ width: 300 }}>
-            <ListItemText primary="Seller Account Details" />
-            {sellerAccount ? <ExpandLessIcon /> : <ExpandMoreIcon />}
-          </ListItem>
-          <Collapse in={sellerAccount} timeout="auto" unmountOnExit>
-            <Divider />
-            <List component="div" disablePadding>
-              <ListItem
-                button
-                component={RouterLink}
-                to="/invoice/seller-account"
-                style={{ width: 300 }}
-              >
-                <ListItemText inset primary="Seller Account" />
-              </ListItem>
-            </List>
-          </Collapse>
-                 {/* Order book */}
-                 <ListItem button onClick={() => setExpandOrderBook(!expandOrderBook)} style={{ width: 300 }}>
-            <ListItemText primary="Order Book" />
-            {expandOrderBook ? <ExpandLessIcon /> : <ExpandMoreIcon />}
-          </ListItem>
-          <Collapse in={expandOrderBook} timeout="auto" unmountOnExit>
-            <Divider />
-            <List component="div" disablePadding>
-              <ListItem
-                button
-                component={RouterLink}
-                to="/invoice/customer-order-book"
-                style={{ width: 300 }}
-              >
-                <ListItemText inset primary="Customer Wise Order Book" />
-              </ListItem>
-                <ListItem
-                button
-                component={RouterLink}
-                to="/invoice/product-order-book"
-                style={{ width: 300 }}
-              >
-                <ListItemText inset primary="Product Wise Order Book" />
-              </ListItem>
-            </List>
-          </Collapse>
+         <ListItems />
         </List>
       </Drawer>
-      <AppBar position="fixed" color="primary" className="not-scrolled">
+      {/* Header */}
+      <AppBar position="fixed" sx={{backgroundColor:"#006ba1"}} className="not-scrolled">
         <Toolbar>
           {auth.user && (
             <IconButton
@@ -372,7 +91,7 @@ export const Header = () => {
             Bold-SoftMation
           </Typography>
           <>
-            {!auth.user ? (
+            {/* {!auth.user ? (
               <>
                 <Button
                   {...{
@@ -380,16 +99,20 @@ export const Header = () => {
                     to: "/login",
                     component: RouterLink,
                   }}
-                  style={{
-                    ...boxStyle,
-                    fontFamily: "Open Sans; sans-serif",
-                    fontWeight: "bold",
-                    size: "18px",
-                    marginLeft: "38px",
-                  }}
-                  color="inherit"
-                  onMouseEnter={handleMouseEnter}
-                  onMouseLeave={handleMouseLeave}
+                  style={
+                    isHoverLogin === true
+                      ? {
+                          backgroundColor: isHoverLogin
+                            ? "rgb(0, 191, 255)"
+                            : "rgb(0, 191, 255)",
+                          fontFamily: "Open Sans; sans-serif",
+                          fontWeight: "bolder",
+                          size: "18px",
+                        }
+                      : null
+                  }
+                  onMouseEnter={() => setIsHoverLogin(true)}
+                  onMouseLeave={() => setIsHoverLogin(false)}
                 >
                   Login
                 </Button>
@@ -399,44 +122,53 @@ export const Header = () => {
                     to: "/signup",
                     component: RouterLink,
                   }}
-                  style={{
-                    ...boxStyle,
-
-                    fontFamily: "Open Sans; sans-serif",
-                    fontWeight: "bold",
-                    size: "18px",
-                    marginLeft: "38px",
-                  }}
+                  style={
+                    isHoverSignup === true
+                      ? {
+                          backgroundColor: isHoverSignup
+                            ? "rgb(0, 191, 255)"
+                            : "rgb(0, 191, 255)",
+                          fontFamily: "Open Sans; sans-serif",
+                          fontWeight: "bolder",
+                          size: "18px",
+                        }
+                      : null
+                  }
                   className="menuButton"
-                  color="inherit"
-                  onMouseEnter={handleMouseEnter}
-                  onMouseLeave={handleMouseLeave}
+                  onMouseEnter={() => setIsHoverSignup(true)}
+                  onMouseLeave={() => setIsHoverSignup(false)}
                 >
                   Signup
                 </Button>
               </>
-            ) : (
+            ) : ( */}
+            {auth.user && 
               <Button
                 {...{
                   color: "inherit",
                   to: "/",
                   component: RouterLink,
                 }}
-                style={{
-                  ...boxStyle,
-                  fontFamily: "Open Sans; sans-serif",
-                  fontWeight: "bold",
-                  size: "18px",
-                  marginLeft: "38px",
-                }}
-                onMouseEnter={handleMouseEnter}
-                onMouseLeave={handleMouseLeave}
-                color="inherit"
+                style={
+                  isHoverLogout === true
+                    ? {
+                        backgroundColor: isHoverLogout
+                          ? "rgb(0, 191, 255)"
+                          : "rgb(0, 191, 255)",
+                        fontFamily: "Open Sans; sans-serif",
+                        fontWeight: "bolder",
+                        size: "18px",
+                      }
+                    : null
+                }
+                onMouseEnter={() => setIsHoverLogout(true)}
+                onMouseLeave={() => setIsHoverLogout(false)}
                 onClick={logout}
               >
                 Logout
               </Button>
-            )}
+}
+            {/* )} */}
           </>
         </Toolbar>
       </AppBar>
